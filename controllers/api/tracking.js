4e88f6aa8845419ffd215d0d19415280d3d49acb
TRASHpanda364 <dixonkimiko@gmail.com>
2024-08-22 19:50:53 -0400

    added routes and deleted files

--- controllers/api/tracking.js
+++ controllers/api/tracking.js
@@ -1,13 +1,23 @@
 const router = require('express').Router();
-const { PortionInCustomMeal,CustomMeal } = require('../../models');
+const { Meal,Portion,Food,PortionInMeal,PortionInCustomMeal,CustomMeal } = require('../../models');
 
 router.post('/portionInCustomMeal', async (req, res) => {
     try {
-
+      const allPortions = req.body.selectedIds.map(id => {
+        
+        return {
+          portion_id:id,
+          custom_meal_id:req.body.customMealId
+          }
+        
+      })
     //bulk create
-      const portionInCustomMeal = await PortionInCustomMeal.create({
+      const portionInCustomMeal = await PortionInCustomMeal.bulkCreate({
+        allPortions
+      })
+      /* const portionInCustomMeal = await PortionInCustomMeal.create({
         ...req.body,
-      });
+      }); */
       res.status(200).json(portionInCustomMeal);
     } catch (err) {
       res.status(400).json(err);
@@ -18,32 +28,84 @@ router.post('/createCustomMeal', async (req, res) => {
     try {
       const customMeal = await CustomMeal.create({
         ...req.body,
-        person_id: req.session.person_id,
+        person_id: req.session.id,
       });
-      res.status(200).json(customMeal);
+      res.render('meals',customMeal)
+      // res.status(200).json(customMeal);
     } catch (err) {
       res.status(400).json(err);
     }
 });
 
 router.post('/createMeal', async (req, res) => {
-    
+  try{
+    const meals = await Meal.bulkCreate(
+      {
+        date:req.body.date,
+        meal_time:'Breakfast',
+        person_id:req.session.id
+      },
+      {
+        date:req.body.date,
+        meal_time:'Lunch',
+        person_id:req.session.id
+      },
+      {
+        date:req.body.date,
+        meal_time:'Dinner',
+        person_id:req.session.id
+      }
+    )
+
+    res.render('homepage',meals)
+  } 
+  catch{
+    res.status(500).json('failed to create meals')
+  } 
+  
 })
 
-  //create portion
-  router.post('/portion', async (req, res) => {
-
-  })
-
-/*   //create
-  router.post('/portion', async (req, res) => {
-    
-  }) */
+// do /:id for foods,foodinfo, meals pages on homeRoutes
+//create portion
+router.post('/:id/portion', async (req, res) => {
+  try{
+    const portion = await Portion.create(
+      /* {
+        include:[{model:Meal}]
+      }, */
+      {
+        portion:req.body.portion, 
+        protein:req.body.protein,
+        carbs:req.body.carbs,
+        fat:req.body.fat,
+        calories:req.body.calories,
+        food_id:req.body.food_id
+      }
+    )
+    // req.session.meal_id = req.params.id
+    req.session.portion_id = portion.id
+    res.status(200).json(portion)
+  }
+  catch{
+    res.status(500).json('failed to create portion')
+  }
+})
 
   //adding the portion to the meal
-  router.post('/portionInMeal', async (req, res) => {
+router.post('/:id/portionInMeal', async (req, res) => {
+  try{
+    const mealPortion = PortionInMeal.create({
+      portion_id:req.params.id/* req.session.meal_id */,
+      meal_id:req.session.portion_id
+    })
+
+    res.status(200).json(mealPortion)
+  }
+  catch{
+    res.status(500).json('failed to add portion to meal')
+  }
     
-  })
+})
 
   //remove portion from meal
   /* router.delete('/portionInMeal', async (req, res) => {
@@ -54,3 +116,4 @@ router.post('/createMeal', async (req, res) => {
   router.delete('/portion', async (req, res) => {
     
   }) */
+
